379d94024f829c32095db725c8018375fdd10d45
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2020-08-31 09:41:57 +0530

    Create templates.cpp

---
+++ templates.cpp
@@ -0,0 +1,39 @@
+/*             OO
+               ||
+               ||
+             __/\__
+          __/\    /\__
+         |__|O    O|__|      ====>"Hello world!"
+           (   /\   )
+            ||||||||
+
+/*******Author:djm_boss*******/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ll mod=1000000007;
+ll inf=INT_MAX;
+int main()
+{
+    ///fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ///file I/O
+   #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+   #endif
+
+   ///test cases(optional)
+   ll t;
+   cin>>t;
+   while(t--)
+   {
+       ///code starts here
+
+   }
+}
+
